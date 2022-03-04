422b589516dac47e6e9161fa91f49e9a4e1d5b12
rakibulhasan933 <rakibulmd933@gmail.com>
2022-03-05 02:25:05 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useState } from 'react';
+
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+  const handleGoogle = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+  };
+  const handleLogOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      setUser({})
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+  const
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {!user.email ? <button onClick={handleGoogle}>Google</button> :
+
+        <button onClick={handleLogOut}>Logout</button>}
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome{user.name} </h2>
+          <p>Your email :{user.email} </p>
+          <img src={user.photo} alt="man" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5_LQ8eFOQhXrya6G96qDrUF2g-MW8YDI",
+    authDomain: "login-authentication-2022.firebaseapp.com",
+    projectId: "login-authentication-2022",
+    storageBucket: "login-authentication-2022.appspot.com",
+    messagingSenderId: "1069407728511",
+    appId: "1:1069407728511:web:254bb2f28f45259da6e04a"
+};
+export default firebaseConfig;
